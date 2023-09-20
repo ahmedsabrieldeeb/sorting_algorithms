de747a71ac291fce3d03c23b3759511bb6f4aebc
Ahmed Eldeeb <eldeeb757575@gmail.com>
2023-09-20 19:13:14 +0300

    edit files

--- print_array.c
+++ print_array.c
@@ -1,28 +1,5 @@
-#include <stdio.h>
 #include <stdlib.h>
-#include "sort.h"
-
-/**
- * print_list - Prints a list of integers
- *
- * @list: The list to be printed
- */
-void print_list(const listint_t *list)
-{
-    int i;
-
-    i = 0;
-    while (list)
-    {
-        if (i > 0)
-            printf(", ");
-        printf("%d", list->n);
-        ++i;
-        list = list->next;
-    }
-    printf("\n");
-}
-
+#include <stdio.h>
 
 /**
  * print_array - Prints an array of integers
