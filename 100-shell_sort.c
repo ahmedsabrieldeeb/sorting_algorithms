cc50106dfe529c1d3ea6bfe4dabd96fc0b7139ba
jomana <69699199+jomanaehabb@users.noreply.github.com>
2023-09-25 14:45:23 +0300

    100-shell_sort.c

---
+++ 100-shell_sort.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include "sort.h"
+
+/**
+ * shell_sort - sorts an array of integers in ascending order using the Shell sort algorithm, using the Knuth sequence
+ * @arr: array
+ * @size: size of array
+ */
+void shell_sort(int *arr, size_t size)
+{
+	int num = 1;
+	int i, j, x;
+
+	while (num <= size / 3)
+		num = num * 3 + 1;
+
+	while (num > 0)
+	{
+		for (i = num; i < size; i++)
+		{
+			x = arr[i];
+			j = i;
+
+			while (j > num - 1 && arr[j - num] >= x)
+			{
+				arr[j] = arr[j - num];
+				j -= num;
+			}
+
+			arr[j] = x;
+		}
+
+		print_array(arr, size);
+		printf("\n");
+
+		num = (num - 1) / 3;
+	}
+}
