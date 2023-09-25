516e24e0db67cbbff4ed03552dfcca2082420605
jomana <69699199+jomanaehabb@users.noreply.github.com>
2023-09-25 19:01:50 +0300

    update

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -5,42 +5,43 @@
  * @a: The first integer to swap.
  * @b: The second integer to swap.
  */
-void swap_ints(int *x, int *y)
+void swap_ints(int *a, int *b)
 {
 	int tmp;
 
-	tmp = *x;
-	*x = *y;
-	*y = tmp;
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
 }
 
 /**
  * shell_sort - Sort an array of integers in ascending
  * @array: An array of integers.
  * @size: The size of the array.
+ *
  * Description: Uses the Knuth interval sequence.
  */
-void shell_sort(int *arr, size_t size)
+void shell_sort(int *array, size_t size)
 {
-	size_t g, i, j;
+	size_t gap, i, j;
 
-	if (arr == NULL || size < 2)
+	if (array == NULL || size < 2)
 		return;
 
-	for (g = 1; g < (size / 3);)
-		g = g * 3 + 1;
+	for (gap = 1; gap < (size / 3);)
+		gap = gap * 3 + 1;
 
-	for (; g >= 1; g /= 3)
+	for (; gap >= 1; gap /= 3)
 	{
-		for (i = g; i < size; i++)
+		for (i = gap; i < size; i++)
 		{
 			j = i;
-			while (j >= g && arr[j - g] > arr[j])
+			while (j >= gap && array[j - gap] > array[j])
 			{
-				swap_ints(arr + j, arr + (j - g));
-				j -= g;
+				swap_ints(array + j, array + (j - gap));
+				j -= gap;
 			}
 		}
-		print_array(arr, size);
+		print_array(array, size);
 	}
 }
