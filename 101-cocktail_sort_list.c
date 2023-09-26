8f3e710868524b6b53076f37a280b275e9bc6426
Ahmed Eldeeb <eldeeb757575@gmail.com>
2023-09-26 05:00:51 +0300

    cocktail sort algo with lists

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -7,38 +7,38 @@
  */
 void cocktail_sort_list(listint_t **list)
 {
-    int swap;
-    listint_t *node;
+	int swap;
+	listint_t *nn;
 
-    if (list == NULL || *list == NULL || (*list)->next == NULL)
-        return;
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
+		return;
 
-    do {
-        swap = 0;
-        for (nn = *list; nn->next != NULL; nn = nn->next)
-        {
-            if (nn->n > nn->next->n)
-            {
-                swap_nodes(list, nn, nn->next);
-                print_list(*list);
-                swap = 1;
-            }
-        }
+	do {
+		swap = 0;
+		for (nn = *list; nn->next != NULL; nn = nn->next)
+		{
+			if (nn->n > nn->next->n)
+			{
+				swap_nodes(list, nn, nn->next);
+				print_list(*list);
+				swap = 1;
+			}
+		}
 
-        if (!swap)
-            break;
+		if (!swap)
+			break;
 
-        swap = 0;
-        for (nn = nn->prev; nn->prev != NULL; nn = nn->prev)
-        {
-            if (nn->n < nn->prev->n)
-            {
-                swap_nodes(list, nn->prev, nn);
-                print_list(*list);
-                swap = 1;
-            }
-        }
-    } while (swap);
+		swap = 0;
+		for (nn = nn->prev; nn->prev != NULL; nn = nn->prev)
+		{
+			if (nn->n < nn->prev->n)
+			{
+				swap_nodes(list, nn->prev, nn);
+				print_list(*list);
+				swap = 1;
+			}
+		}
+	} while (swap);
 }
 
 /**
@@ -49,16 +49,16 @@ void cocktail_sort_list(listint_t **list)
  */
 void swap_nodes(listint_t **list, listint_t *n1, listint_t *n2)
 {
-    if (n1->prev != NULL)
-        n1->prev->next = n2;
-    else
-        *list = n2;
+	if (n1->prev != NULL)
+		n1->prev->next = n2;
+	else
+		*list = n2;
 
-    if (n2->next != NULL)
-        n2->next->prev = n1;
+	if (n2->next != NULL)
+		n2->next->prev = n1;
 
-    n1->next = n2->next;
-    n2->prev = n1->prev;
-    n1->prev = n2;
-    n2->next = n1;
+	n1->next = n2->next;
+	n2->prev = n1->prev;
+	n1->prev = n2;
+	n2->next = n1;
 }
