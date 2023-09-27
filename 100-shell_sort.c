d6a52cebc3a46160b1ddd0e4c61a31203b5d82f3
Ahmed Eldeeb <eldeeb757575@gmail.com>
2023-09-28 01:44:47 +0300

    implemented shell sort using knuth sequence

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -1,28 +1,72 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "sort.h"
+
+void swap(int *first, int *second);
+
+
 /**
-  * shell_sort - shell sort, knuth sequence
+  * shell_sort - shell sort using gap conecpt by knuth sequence
+  *
+  * Description:
+  * --> shell sort is general case of insertion sort
+  * ... when gap is equal to 1, it really becomes insertion sort
+  *
+  * --> Knuth sequence is:
+  * ... n+1 = n * 3 + 1, whereas n is the size of array
+  * ... 1, 4, 13, 40, 121, ...
+  *
+  * --> expected to print the array each time you decrease the interval
+  *
   * @array: array to be sorted
   * @size: size of array
   */
-void shell_sort(int *arr, size_t size)
+void shell_sort(int *array, size_t size)
 {
-	size_t k, i, j;
-	int tmp;
+	size_t gap, idx, temp;
 
-	k = 1;
-	while (k < size)
-		k = (k * 3) + 1;
-	k = (k - 1) / 3;
-	while (k > 0)
+	/* getting maximum value of gap to start algo with */
+	gap = 1;
+	while ((gap * 3 + 1) <= size)
+		gap = gap * 3 + 1;
+
+	/* iterating all gaps possible */
+	while (gap >= 1)
 	{
-		for (i = k; i < size; i++)
+		idx = 0;
+		/* sorting in range of allowed gap */
+		while (idx < size && idx + gap < size)
 		{
-			tmp = arr[i];
-			for (j = i; j >= k && array[j - k] > tmp; j -= k)
-				arr[i2] = arr[j - k];
-			arr[i2] = tmp;
+			/* sorting in forward direction */
+			if (array[idx] > array[idx + gap])
+
+				swap(array + idx, array + idx + gap);
+
+			/* sorting in backward direction */
+			temp = idx;
+			while ((int)temp - (int)gap >= 0)
+			{
+				if (array[temp] < array[temp - gap])
+					swap(array + temp, array + temp - gap);
+				temp--;
+			}
+			idx++;
 		}
-		k = (k - 1) / 3;
-		print_array(arr, size);
+		gap = (gap - 1) / 3; /* reduce gap based on knuth sequence */
+		print_array(array, size);
 	}
+}
+/**
+ * swap - swapping two elementes
+ *
+ * @first: first element
+ * @second: second element
+*/
+void swap(int *first, int *second)
+{
+	int temp;
+
+	temp = *first;
+	*first = *second;
+	*second = temp;
 }
--- sort.h
+++ sort.h
@@ -23,6 +23,7 @@ void print_array(const int *array, size_t size);
 /* sorting algos */
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
+void shell_sort(int *array, size_t size);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void quick_sort_hoare(int *array, size_t size);
